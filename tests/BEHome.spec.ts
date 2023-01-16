ef6de9aa4d1155a6096bb757ced7183bc9efdaa8
teddorian <bogdan@onix-systems.com>
2023-01-16 17:26:36 +0200

    change BEHome description

--- tests/BEHome.spec.ts
+++ tests/BEHome.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.describe('BrandExtender Home', () => {
-  test('should allow me visit Home page', async ({ page }) => {
+  test('should allow me visit Home page and click Connect wallet', async ({ page }) => {
     await page.goto('https://brandextender.io/demo/');
     await page.locator('//button[text()="Connect Wallet"]').click();
     const locator = page.locator('//h4[text()="Connect your wallet"]');
